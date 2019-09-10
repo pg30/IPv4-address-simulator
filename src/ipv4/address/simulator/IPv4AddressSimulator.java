9f0275aec49110dc7b16a154d02cb70ec91f5e96
Pranay Garg <pranaygarg001@gmail.com>
2019-09-11 01:31:12 +0530

    GUI Started

--- src/ipv4/address/simulator/IPv4AddressSimulator.java
+++ src/ipv4/address/simulator/IPv4AddressSimulator.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -16,6 +16,8 @@ public class IPv4AddressSimulator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        MainUI start = new MainUI();
+        start.setVisible(true);
     }
     
 }
